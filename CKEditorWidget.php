45e1c8db36c86e3b08c46f821e7d2307095e9fa7
Firs Yuriy <firs.yura@gmail.com>
2013-01-10 20:31:07 +0200

    Init commit

---
+++ CKEditorWidget.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Simple Yii CKEditor widget
+ * @property string $configJS
+ * @property string $assetsPath
+ * @property string $assetsUrl
+ * @author Yuriy Firs <firs.yura@gmail.com>
+ * @version 0.1
+ */ 
+class CKEditorWidget extends CInputWidget {
+    /**
+     * Assets package ID.
+     */
+    const PACKAGE_ID = 'ckeditor-widget';
+
+    /**
+     * @var array Default config
+     */
+    public $config = array(
+        // You can set your default config
+        //'language' => 'ru',
+    );
+
+    public $package = array();
+
+    /**
+     * Init widget.
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->package = array(
+            'baseUrl' => $this->assetsUrl,
+            'js' => array(
+                'ckeditor.js',
+            )
+        );
+
+        $this->registerClientScript();
+    }
+
+    /**
+     * Register CSS and Script.
+     */
+    protected function registerClientScript()
+    {
+        Yii::app()->clientScript
+            ->addPackage(self::PACKAGE_ID, $this->package)
+            ->registerPackage(self::PACKAGE_ID)->registerScript(
+                $this->id,
+                "CKEDITOR.editorConfig = function( config ) {
+                ".$this->configJS."
+                };",
+                CClientScript::POS_HEAD
+            );
+    }
+
+    /**
+     * Get the assets path.
+     * @return string
+     */
+    public function getAssetsPath()
+    {
+        return __DIR__ . '/assets';
+    }
+
+    /**
+     * Print activeTextArea
+     */
+    public function run()
+    {
+        // add class ckeditor
+        if (strpos($this->htmlOptions['class'], 'ckeditor')===false) {
+            $this->htmlOptions['class'] .= ' ckeditor';
+        }
+        echo CHtml::activeTextArea($this->model,$this->attribute,$this->htmlOptions);
+    }
+
+    /**
+     * Publish assets and return url.
+     * @return string
+     */
+    public function getAssetsUrl()
+    {
+        return Yii::app()->assetManager->publish($this->assetsPath);
+    }
+
+    /**
+     * Convert config array to config string
+     * @return string
+     */
+    public function getConfigJS()
+    {
+        $return = '';
+        foreach ($this->config as $key=>$value) {
+            $return .= "config.".$key." = '".$value."'; ";
+        }
+        return $return;
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-yii-ckeditor
+CKEditor widget for Yii Framework
 ============
 
-Simple Yii CKEditor widget
+Simple Yii CKEditor widget.
+CKEditor - WYSIWYG editor for everyone.
+Widget generate CHtml::activeTextArea with same properties.
+Tested with CKEditor 4+ version.
+
+Installation
+------------
+
+Copy this widget to Extensions folder.
+Download CKEditor from http://ckeditor.com/download
+Paste CKEditor's files to extension assets folder.
+
+Usage
+-----
+
+First, import the widget class file
+
+```php
+Yii::import('ext.yii-ckeditor.CKEditorWidget');
+```
+
+Next, call the widget:
+
+```php
+$this->widget('CKEditorWidget', array(
+	'model' => $model,
+	'attribute' => 'field',
+	// editor options http://docs.ckeditor.com/#!/api/CKEDITOR.config
+	'config' => array(
+		'language' => 'ru',
+	),
+));
+```
